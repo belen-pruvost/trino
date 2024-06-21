5cc9ea2eee74ff0bd67da3b499f95235750cc7b9
kasiafi <30203062+kasiafi@users.noreply.github.com>
2024-06-21 10:01:22 +0200

    Fix table function execution with row semantics
    Previously, when table function did not declare partitioning (row semantics),
    it would adopt the source partitioning on the global level, but on a worker node
    it would run single-threaded and first buffer all data in memory, like one big partition.
    After the change, the local execution processes input pages in a streaming fashion.
    
    After: https://github.com/trinodb/trino/pull/21378

--- core/trino-main/src/main/java/io/trino/sql/planner/LocalExecutionPlanner.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/LocalExecutionPlanner.java
@@ -1782,10 +1782,9 @@ public class LocalExecutionPlanner
                 }
             }
 
-            List<Integer> partitionChannels = node.getSpecification()
+            Optional<List<Integer>> partitionChannels = node.getSpecification()
                     .map(DataOrganizationSpecification::partitionBy)
-                    .map(list -> getChannelsForSymbols(list, source.getLayout()))
-                    .orElse(ImmutableList.of());
+                    .map(list -> getChannelsForSymbols(list, source.getLayout()));
 
             List<Integer> sortChannels = ImmutableList.of();
             List<SortOrder> sortOrders = ImmutableList.of();
